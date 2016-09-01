35a4707229526e286bc13f8adcb66aba69183e1b
Anna Dolan <anna.dolan@gmail.com>
2016-09-01 08:24:26 -0600

    added tests for Enigma class

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -10,12 +10,14 @@ class EnigmaTest < Minitest::Test
     assert_instance_of Enigma, e
   end
 
-  def test_encrypts_with_input_key_and_date
+  def test_encrypts_with_input_key_and_date_returns_different_string_of_same_length
     e = Enigma.new
-    assert_equal "cfykj qzmladnt lt bky sqdfac..rmy..", e.encrypt("hello darkness my old friend..end..", "12345", Date.today)
+    assert_equal 35, e.encrypt("hello darkness my old friend..end..", "12345", Date.today).length
+    assert_equal String, e.encrypt("hello darkness my old friend..end..", "12345", Date.today).class
+    refute_equal "hello darkness my old friend..end..", e.encrypt("hello darkness my old friend..end..", "12345", Date.today)
   end
 
-  def test_encrypts_with_input_only
+  def test_encrypts_with_input_only_returns_different_string_of_same_length
     e = Enigma.new
     assert_equal 35, e.encrypt("hello darkness my old friend..end..").length
     refute_equal "hello darkness my old friend..end..", e.encrypt("hello darkness my old friend..end..")
@@ -23,21 +25,23 @@ class EnigmaTest < Minitest::Test
 
   def test_decrypts_with_input_key_and_date
     e = Enigma.new
-    assert_equal "hello darkness my old friend..end..", e.decrypt("cfykj qzmladnt lt bky sqdfac..rmy..", "12345", Date.today)
+    assert_equal "hello darkness my old friend..end..", e.decrypt(e.encrypt("hello darkness my old friend..end..", "12345", Date.today), "12345", Date.today)
   end
 
   def test_decrypts_with_input_and_key_only
     e = Enigma.new
-    assert_equal "hello darkness my old friend..end..", e.decrypt("cfykj qzmladnt lt bky sqdfac..rmy..", "12345")
+    assert_equal "hello darkness my old friend..end..", e.decrypt(e.encrypt("hello darkness my old friend..end..", "12345", Date.today), "12345")
   end
 
-  def test_cracks_with_input_and_date
+  def test_cracks_with_input_and_date_returns_string_of_same_length_ending_in_end
     e = Enigma.new
-    assert_equal "hello darkness my old friend..end..", e.crack("cfykj qzmladnt lt bky sqdfac..rmy..", Date.today)
+    assert_equal 35, e.crack(e.encrypt("hello darkness my old friend..end..", "12345", Date.today)).length
+    assert_equal "..end..", e.crack(e.encrypt("hello darkness my old friend..end..", "12345", Date.today), Date.today)[-7..-1]
   end
 
-  def test_cracks_with_input_only
+  def test_crack_with_input_only_returns_string_of_same_length_ending_in_end
     e = Enigma.new
-    assert_equal "hello darkness my old friend..end..", e.crack("cfykj qzmladnt lt bky sqdfac..rmy..")
+    assert_equal 35, e.crack(e.encrypt("hello darkness my old friend..end..", "12345", Date.today)).length
+    assert_equal "..end..", e.crack(e.encrypt("hello darkness my old friend..end..", "12345", Date.today))[-7..-1]
   end
 end
